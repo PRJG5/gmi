4b063e8295b948f94470a0aed0d4c8412b3fc785
Corentin <magerotte.corentin@gmail.com>
2019-10-24 15:09:54 +0200

    Add tests and comment the code.

--- app/Note.php
+++ app/Note.php
@@ -4,11 +4,14 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Model from Eloquent representing a Note who belongs to a Card model object.
+ */
 class Note extends Model
 {
 
     protected $fillable = ['description'];
-    
+
     public function card(){
         return $this->$belongsTo(Card::class);
     }
--- database/migrations/2019_10_24_121212_create_notes_table.php
+++ database/migrations/2019_10_24_121212_create_notes_table.php
@@ -4,18 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+/**
+ * Note migration representing the Note table.
+ */
 class CreateNotesTable extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
     public function up()
     {
         Schema::create('notes', function (Blueprint $table) {
+            //Note's id
             $table->bigIncrements('id');
+            //Note's description
             $table->text('description')->nullable($value=True);
+            //Note creation and update
             $table->timestamps();
         });
     }
