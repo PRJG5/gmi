bfad0c65929c35ba2f00ff7718ddebe842d24cdb
43121 <43121@etu.he2b.be>
2019-10-24 14:29:44 +0200

    Add base layout and users

---
+++ database/migrations/2019_10_17_144835_create_speaked_languages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSpeakedLanguagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('speakedLanguages', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id');
+            $table->string('language_id');
+            $table->unique('user_id' , 'language_id');
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('language_id')->references('id')->on('languages');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('speakedLanguages');
+    }
+}
