51b1dd66362aa37812b30b0c060e57c1e2896e21
Corentin <magerotte.corentin@gmail.com>
2019-10-24 14:43:49 +0200

    Add fillable in model for mass assignement

--- app/Note.php
+++ app/Note.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Note extends Model
 {
+
+    protected $fillable = ['description'];
+    
     public function card(){
         return $this->$belongsTo(Card::class);
     }
