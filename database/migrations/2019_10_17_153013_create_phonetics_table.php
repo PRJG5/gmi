52e3840e1eae9f65f8cc348fe8413dc910b84a6f
44424 <44424@etu.he2b.be>
2019-10-24 17:35:19 +0200

    ajout phonetic en cours test a refaire

---
+++ database/migrations/2019_10_17_153013_create_phonetics_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePhoneticsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('phonetics', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            // $table->foreign('card')->
+            // references('id')->
+            // on('cards')->
+            // onDelete('cascade');
+            $table->string('textDescription');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('phonetics');
+    }
+}
