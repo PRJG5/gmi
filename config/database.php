c62a54ffcd5fa12bf0d509ba503bed92079b1d0b
Gosset Quentin <49923@etu.he2b.be>
2019-10-24 15:20:31 +0200

    add heroku config

--- config/database.php
+++ config/database.php
@@ -1,6 +1,16 @@
 <?php
 
 use Illuminate\Support\Str;
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "ecole";
+$url  = parse_url(getenv("CLEARDB_DATABASE_URL"));
+@$host = $url["host"];
+@$username = $url["user"];
+@$password = $url["pass"];
+@$database = substr($url["path"], 1);
+
 
 return [
 
