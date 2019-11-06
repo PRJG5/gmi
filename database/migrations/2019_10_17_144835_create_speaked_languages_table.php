d8c88ace9ffbb7e47e91ed1a96132563f069faf1
Gosset Quentin <49923@etu.he2b.be>
2019-11-06 15:23:55 +0100

    database update

--- database/migrations/2019_10_17_144835_create_speaked_languages_table.php
+++ database/migrations/2019_10_17_144835_create_speaked_languages_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSpokenLanguagesTable extends Migration
+class CreateSpeakedLanguagesTable extends Migration
 {
     /**
      * Run the migrations.
