c934cc84fbf8db7684452eefe02455b970ba0d79
44424 <44424@etu.he2b.be>
2019-11-06 15:54:00 +0100

    comment route  factory

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/card/create', function () {
     // Use Auth::user() and not facory
-    $user = factory('App\User')->make();
+    //$user = factory('App\User')->make();
     $data = ['user' => Auth::user(), 'languages' => Language::getInstances(), 'editable' => True, 'card' => factory('App\Card')->make()];
     return view('card/create',$data);
 });
