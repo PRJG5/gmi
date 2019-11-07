a979b977537451484adc1d29787e38f572d3d5d9
43121 <43121@etu.he2b.be>
2019-11-07 17:58:27 +0100

    Add link in menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -63,6 +63,8 @@
                                         {{ __('Logout') }}
                                     </a>
 
+                                    <a class="dropdown-item" href="/searchByUser">Recherche fiche par auteur</a>
+
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
