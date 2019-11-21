4731a136c6ad96a4651f11ca25841e7dd1ebde5d
Loicsth <45242@etu.he2b.be>
2019-11-21 16:52:35 +0100

    Rajout des tests d'ecran recherche fiche

---
+++ tests/Feature/SearchCardTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use App\Card;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class SearchCardTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test1()
+    {
+        $card1 = Card::create([
+            "heading" => "Persona",
+            "language_id" => "EN"]);
+        $card2 = Card::create([
+            "heading" => "Shadow",
+            "language_id" => "FR"])->create();
+        $card3 = Card::create([
+            "heading" => "Hello",
+            "language_id" => "HUY"])->create();
+        $card4 = Card::create([
+            "heading" => "Heyololo",
+            "language_id" => "FR"])->create();
+        $card5 = Card::create([
+            "heading" => "Pouloulou",
+            "language_id" => "EN"])->create();
+
+        $user = User::create([
+            "name" => "lolo",
+            "email" => "loic.corbion@gmail.com",
+            "password" => "lolofire"
+        ]);
+        $this->actingAs($user);
+        $response = $this->get('/searchCard');
+        $response->assertSeeText("Shadow");
+        $response->assertSeeText("Persona");
+        $response->assertSeeText("Heyololo");
+        $response->assertSeeText("Pouloulou");
+        $this->get('/searchCard?search=Shadow&languages=All');
+        $response->assertSeeText("Shadow");
+        $response->assertSeeText("FR");
+
+        //testAllLanguageAllHeading();
+
+        $response = $this->get('/searchCard?search=&languages=All');
+        $response->assertSeeText("Shadow");
+        $response->assertSeeText("Persona");
+        $response->assertSeeText("Heyololo");
+        $response->assertSeeText("Pouloulou");
+
+        
+        
+        //testDontSeeLanguage();
+
+        $response = $this->get('/searchCard?search=&languages=FR');
+        $response->assertSeeText("Shadow");
+        $response->assertSeeText("Heyololo");
+        $response->assertDontSeeText("Pouloulou");
+        $response->assertDontSeeText("Persona");
+
+        $response->assertStatus(200);
+    }
+
+
+    
+}
