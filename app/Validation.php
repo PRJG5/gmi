686eb58274ebb623514340b475fe4449dce6ef98
49222 <49222@etu.he2b.be>
2019-11-21 16:43:07 +0100

    init Validation

---
+++ app/Validation.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Validation extends Model
+{
+
+
+    protected $fillable = [
+        'voteNb',
+        'userNb',
+        'validationRate',
+        'validated_at'
+	];
+    
+
+    public function __toString(){
+        return $this->id
+        . " [votes:" . $this->voteNb
+        . ", users:" . $this->userNb
+        . ", validation rate:" . $this->validationRate
+        . "%, validated at:" . $this->validated_at
+        . "]"; 
+   }
+}
---
+++ database/migrations/2019_11_21_142938_create_validations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateValidationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('validations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('voteNb');
+            $table->unsignedInteger('userNb');
+            $table->unsignedTinyInteger('validationRate');
+            $table->date('validated_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('validations');
+    }
+}
