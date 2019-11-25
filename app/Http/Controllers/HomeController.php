96703d31402b2c29ff8afe6a5a58404d9540e887
BrandtMatthieu <44422@etu.he2b.be>
2019-11-25 02:30:42 +0100

    small fixes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,27 +6,28 @@ use App\User;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		$this->middleware('auth');
+	}
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
-    public function index()
-    {
-        return view('home');
-    }
+	/**
+	 * Show the application dashboard.
+	 *
+	 * @return \Illuminate\Contracts\Support\Renderable
+	 */
+	public function index() {
+		return view('home');
+	}
 
-    public function indexUsers(){
-        return view('auth.administration.users')->with(['users'=> User::all()]);
-    }
+	public function indexAllUsers() {
+		return view('administration.allUsers', [
+			'users'=> User::all(),
+		]);
+	}
 }
