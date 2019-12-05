b28e4e1eca28d8c6ae2d3c4dc24fca7d6cc88ba9
Cedric Thonus <cdthonus@gmail.com>
2019-12-05 14:59:55 +0100

    add css for create card

--- resources/views/layouts/card.blade.php
+++ resources/views/layouts/card.blade.php
@@ -1,3 +1,5 @@
+@extends("layouts.app")
+@section('content')
 <div class="container p-4">
         <div class="row justify-content-center">
                 <div class="col-md-8">
@@ -8,3 +10,4 @@
                 </div>
         </div>
 </div>
+@endsection
