25e128b188a5c60df004fb7f6ce3359e0dc2a289
Quentin Gosset <quentingosset7500@hotmail.com>
2019-12-05 21:11:33 +0100

    ADD ONDELETE CASCADE ON CARDS FOREIGNKEY

--- database/migrations/2019_11_28_154918_add_validation_to_card.php
+++ database/migrations/2019_11_28_154918_add_validation_to_card.php
@@ -13,7 +13,7 @@ class AddValidationToCard extends Migration
     {
         Schema::table('cards', function (Blueprint $table) {
             $table->unsignedBigInteger('validation_id');
-            $table->foreign('validation_id')->references('id')->on('validations')->onDelete('cascade');;
+            $table->foreign('validation_id')->references('id')->on('validations')->onDelete('cascade');
         });
     }
     /**
