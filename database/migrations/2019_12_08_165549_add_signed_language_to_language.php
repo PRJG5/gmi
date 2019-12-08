7a8f641fbcd6581f08c36a2621f7c60c17d8d30e
43121 <43121@etu.he2b.be>
2019-12-08 18:33:20 +0100

    Add a comumn isSigned in languages

---
+++ database/migrations/2019_12_08_165549_add_signed_language_to_language.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class AddSignedLanguageToLanguage extends Migration
+{
+    /**
+     * Run the migrations. Add a column isSigned to the table 'languages'. 
+     * The column is boolean, to true if the language is signed, false otherwise.
+     * The migration update any column with a slug equals to 'SSI', 'LSBF' or 'LSBN' to true 
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Add the column
+        Schema::table('languages', function (Blueprint $table) {
+            $table->boolean("isSigned")->default(false);
+        });
+        
+        //Update the rows
+        DB::table('languages')
+            ->where('slug', 'SSI')
+            ->orWhere('slug', 'LSBF')
+            ->orWhere('slug', 'LSBN')
+            ->update(['isSigned' => true]);
+
+    }
+
+    /**
+     * Reverse the migrations. Nothing to do
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('languages', function (Blueprint $table) {
+            $table->dropColumn('isSigned');
+        });
+    }
+}
