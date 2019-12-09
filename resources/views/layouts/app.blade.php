ddb44a509a525e0f70e013f6d99257ebaccff3f0
Loic <45242@etu.he2b.be>
2019-12-09 13:35:35 +0100

    Amélioration du navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
     <script type="text/javascript" src="{{ asset('js/jquery.js') }}"></script>
 
     <!--<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script> -->
-   <!-- <script src="{{ asset('js/Bootstrap/bootstrap.min.js') }}"></script> -->
+    <!-- <script src="{{ asset('js/Bootstrap/bootstrap.min.js') }}"></script> -->
     <script src="{{ asset('js/Bootstrap/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('js/mdb.js') }}"></script>
     <script src="{{ asset('js/addons/datatables.min.js') }}"></script>
@@ -42,12 +42,23 @@
 
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
+
+        <!--Navbar -->
+        <nav class="mb-1 navbar navbar-expand-md navbar-dark info-color">
+
+            <a class="navbar-brand" href="{{ url('/') }}">
+                {{ config('app.name', 'GMI') }}
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-4" aria-controls="navbarSupportedContent-4" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent-4">
                 <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="{{ url('/') }}">Home
+                            <span class="sr-only">(current)</span>
+                        </a>
+                    </li>
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('mesFiches') }}" onclick="event.preventDefault();
                                                      document.getElementById('mesFiches-form').submit();">
@@ -56,83 +67,94 @@
                     </li>
 
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Recherche par</a>
-
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Recherche
+                        </a>
+                        <div class="dropdown-menu dropdown-default" aria-labelledby="navbarDropdownMenuLink-333">
                             @if(Auth::user() && Auth::user()->role == \App\Enums\Roles::ADMIN)
                             <a class="dropdown-item" href="/searchByUser">Auteur</a>
                             @endif
                             <a class="dropdown-item" href="/searchCard">Vedette/Langue</a>
                         </div>
                     </li>
-
-
                 </ul>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+                <ul class="navbar-nav ml-auto nav-flex-icons">
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+                    @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                    @if (Route::has('register'))
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    </li>
+                    @endif
+                    @else
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-4" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="fas fa-search"></i>Recherche
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right dropdown-info" aria-labelledby="navbarDropdownMenuLink-4">
+                            <a class="dropdown-item" href="{{ route('mesFiches') }}" onclick="event.preventDefault();
+                                                     document.getElementById('mesFiches-form').submit();">
+                                {{ __('Mes Fiches') }}
+                            </a>
+                            <form id="mesFiches-form" action="{{ route('mesFiches') }}" method="GET" style="display: none;">
+                                @csrf
+                            </form>
+                            <a class="dropdown-item" href="/searchCard">Recherche fiche par vedette/langue</a>
+                            @if(Auth::user() && Auth::user()->role == \App\Enums\Roles::ADMIN)
+                            <a class="dropdown-item" href="/searchByUser">Recherche fiche par auteur</a>
+                            @endif
 
-                    </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                        @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                        @endif
-                        @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ \App\Enums\Roles::getDescription(Auth::user()->role) }} : {{ Auth::user()->name }} <span class="caret"></span>
+
+
+                        </div>
+
+
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-4" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="fas fa-user"></i>Profil
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right dropdown-info" aria-labelledby="navbarDropdownMenuLink-4">
+                            <h6 class="dropdown-header">{{ \App\Enums\Roles::getDescription(Auth::user()->role) }} : {{ Auth::user()->name }}</h6>
+                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                    @if (Auth::user()->role == \App\Enums\Roles::ADMIN)
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-4" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="fas fa-cog"></i>Administration
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-right dropdown-info" aria-labelledby="navbarDropdownMenuLink-4">
+
+                            <a class="dropdown-item" href="{{ route('ListingUsers') }}">
+                                {{ __('Roles') }}
+                            </a>
+                            <a class="dropdown-item" href="{{ route('basicData') }}">
+                                Ajout domaines/sous-domaines/langues
                             </a>
 
 
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-                                @if(Auth::user() && Auth::user()->role == \App\Enums\Roles::ADMIN)
-                                <a class="dropdown-item" href="/searchByUser">Recherche fiche par auteur</a>
-                                @endif
-                                <a class="dropdown-item" href="{{ route('mesFiches') }}" onclick="event.preventDefault();
-                                                     document.getElementById('mesFiches-form').submit();">
-                                    {{ __('Mes Fiches') }}
-                                </a>
-                                @if (Auth::user()->role == \App\Enums\Roles::ADMIN)
-                                <a class="dropdown-item" href="{{ route('ListingUsers') }}">
-                                    {{ __('Roles') }}
-                                </a>
-                                <a class="dropdown-item" href="{{ route('basicData') }}">
-                                    Ajout domaines/sous-domaines/langues
-                                </a>
-                                @endif
-                                <a class="dropdown-item" href="/searchCard">Chercher fiche</a>
+                        </div>
+                    </li>
+                    @endif
 
-                                <form id="mesFiches-form" action="{{ route('mesFiches') }}" method="GET" style="display: none;">
-                                    @csrf
-                                </form>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                        @endguest
-                    </ul>
-                </div>
+                    @endguest
+                </ul>
             </div>
+
         </nav>
+
 
         <main class="py-4">
             <div class="container" style="max-width: 960px">
