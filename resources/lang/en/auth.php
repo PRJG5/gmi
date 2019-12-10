44acd3a1abe3895e2534c6716ef7417f292d7fd8
BrandtMatthieu <44422@etu.he2b.be>
2019-12-10 05:00:03 +0100

    Added translations

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -1,6 +1,13 @@
 <?php
 return [
+	'confirmPassword' => 'Confirm Password',
+	'confirmPasswordBreforeContinuing' => 'Please confirm your password before continuing.',
+	'forgotPassword' => 'I forgot my password',
+	'login' => 'Login',
+	'logout' => 'Logout',
+	'password' => 'Password',
+	'register' => 'Register',
+	'rememberMe' => 'Stay signed in',
     'failed' => 'These credentials do not match our records.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
-]
-?>
+];
