18f8c9c6bbf0127b8084de01f4520e277a71adc2
Loic <45242@etu.he2b.be>
2019-12-11 21:02:55 +0100

    début des password

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,9 @@ use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use PhpParser\Node\Expr\Cast\Object_;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -31,4 +34,70 @@ class UserController extends Controller
         }
     }
 
+    public function modifyPassword(Request $request){
+        
+        if (!(Hash::check($request->get('oldPassword'), Auth::user()->password))) {
+            // The passwords matches
+            return redirect()->back()->with("errorPswd","Your current password does not matches with the password you provided. Please try again.");
+        }
+
+        if(strcmp($request->get('oldPassword'), $request->get('password')) == 0){
+            //Current password and new password are same
+            return redirect()->back()->with("errorPswd","New Password cannot be same as your current password. Please choose a different password.");
+        }
+
+        $validatedData = $request->validate([
+            'oldPassword' => 'required',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        //Change Password
+        $user = User::find(Auth::user()->id);
+        $user->password = bcrypt($request->get('password'));
+        $user->save();
+
+        return redirect()->back()->with("Passwordsuccess","Password changed successfully !");
+
+    }
+    public function modifyMail(Request $request){
+        $mail = $request->email;
+        // return Validator::make($request, [
+            
+        //     'email' => ['required', 'string', 'email', 'max:255', 'unique:users']
+            
+        // ]);
+
+        try {
+            $user = User::find(Auth::user()->id);
+            $user->email=$mail;
+           
+            $user->save();
+            
+        }catch(QueryException $e){
+           
+           return redirect("modifyProfile")->withErrors(["email" => "E mail déjà utilisé"]);
+
+        }
+        return redirect('modifyProfile')->with('validEmail', 'succès');
+       
+
+    }
+    public function modifyLanguages(Request $request){
+        
+    }
+
+    public function admin_credential_rules(Request $data){
+         $messages = [
+            'oldPassword.required' => 'Please enter current password',
+            'password.required' => 'Please enter password',
+        ];
+
+        $validator = Validator::make($data, [
+            'oldPassword' => 'required',
+            'password' => 'required|same:password',
+            'passwordConfirm' => 'required|same:password',     
+        ], $messages);
+
+        return $validator;
+    }  
 }
--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,16 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/searchCard' , 'HomeController@searchCard')->middleware('auth');
 Route::get('/mesFiches', 'MyCardController@index')->name('mesFiches')->middleware('auth');
 Route::get('/users','HomeController@indexUsers')->name('ListingUsers')->middleware(['auth','admin']);
 Route::get('/modifyProfile', 'HomeController@modifyProfile')->name('Modifier son profil')->middleware('auth');
+Route::post('/modifyMail', 'UserController@modifyMail')->name('modifyMail')->middleware('auth');
+Route::post('/modifyLanguages', 'UserController@modifyLanguages')->name('modifyLanguages')->middleware('auth');
+Route::post('/modifyPassword', 'UserController@modifyPassword')->name('modifyPassword')->middleware('auth');
+
 
 /**
  * Cards shouldn't be accessible directly from the web
