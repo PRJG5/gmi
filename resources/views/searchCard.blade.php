c73c10a2f92deaa991b33f9b738f8c47a379f553
Gosset Quentin <49923@etu.he2b.be>
2019-12-12 15:11:05 +0100

    FIX BUG 3-4-15

--- resources/views/searchCard.blade.php
+++ resources/views/searchCard.blade.php
@@ -7,14 +7,18 @@
 	<form class="form-inline" action="/searchCard" method="get">
 
 		<div class="input-group mr-sm-2 w-50">
-			<input name="search" type="search" class="form-control" placeholder="@lang('cards.heading')"/>
+			<input name="search" type="search" class="form-control" placeholder="@lang('cards.heading')" value="{{Request::get('search')}}"/>
 		</div>
 
 		<div class="input-group mr-sm-2 w-25">
 			<select name="languages" class="form-control">
 				<option>All</option>
 				@foreach($languages as $language)
-					<option value="{{$language->slug}}">{{$language->content}}</option>
+					@php if(Request::get('languages') == $language->slug){
+						echo "<option value='$language->slug' selected>$language->content</option>";
+					}else{
+						echo "<option value='$language->slug'>$language->content</option>";
+					} @endphp
 				@endforeach
 			</select>
 		</div>
