683e2aaa886fd1028efa24bfbd5c972484a759e4
Matthieu Brandt <44422@etu.he2b.be>
2019-12-12 05:21:41 +0100

    Nu in het Nederlands

---
+++ resources/lang/nl/auth.php
@@ -0,0 +1,12 @@
+<?php
+return [
+	'confirmPassword' => 'Wachtwoord bevestigen',
+	'forgotPassword' => 'Ik ben mijn wachtwoord vergeten',
+	'login' => 'Aanmelden',
+	'logout' => 'Uitloggen',
+	'password' => 'Wachtwoord',
+	'register' => 'Inscrijven',
+	'rememberMe' => 'Ingelogd blijven',
+    'failed' => 'Deze login gegevens zijn niet correct.',
+    'throttle' => 'Te veel login pogingen. Gelieve in :seconds seconden nogeens proberen.',
+];
